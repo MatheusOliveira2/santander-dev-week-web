41effac9a2e249994cb8b90c52cdfc710a83e9ac
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-29 17:34:33 -0300

    Dashboard Completo

--- src/app/shared/models/stock-model.ts
+++ src/app/shared/models/stock-model.ts
@@ -7,16 +7,17 @@ class Stock{
   variation:	number;
   date:	string;
 
-  constructor(id: number, name: string, price: number, minimum: number,
-    exclusiveMinimum:boolean, variation:number, date:string){
-   this.id = id;
-   this.name = name;
-   this.price = price;
-   this.minimum = minimum;
-   this.exclusiveMinimum = exclusiveMinimum;
-   this.variation = variation;
-   this.date = date;
- };
+//   constructor(id: number, name: string, price: number, minimum: number,
+//     exclusiveMinimum:boolean, variation:number, date:string){
+//    this.id = id;
+//    this.name = name;
+//    this.price = price;
+//    this.minimum = minimum;
+//    this.exclusiveMinimum = exclusiveMinimum;
+//    this.variation = variation;
+//    this.date = date;
+//  };
+
 }
 
 export default Stock;
